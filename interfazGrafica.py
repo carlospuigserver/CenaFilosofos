49f91d917af2db9fd783ba309d718d0c100ca8af
carlospuigserver <cpuiggut@myuax.com>
2023-03-15 14:08:57 +0100

    funcion obtener palillos

--- interfazGrafica.py
+++ interfazGrafica.py
@@ -18,14 +18,24 @@ class Filosofo:
         self.comidas_label= tk.Label(self.interfaz, text=f"Comidas: {self.comidas}")
         self.comidas_label.place(x=self.x+180,y=self.y)
 
-def pensar(self):
-    self.estado="Pensando"
-    self.estado_label.config(text=f"{self.nombre} : {self.estado}")
-    time.sleep(random.uniform(1,5))
+    def pensar(self):
+      self.estado="Pensando"
+      self.estado_label.config(text=f"{self.nombre} : {self.estado}")
+      time.sleep(random.uniform(1,5))
 
 
-def intentar_obtener_palillo(self,palillo):
-    palillo.acquire()
-    self.estado= f"Obtuvo {palillo}"
-    self.estado_label.config(text=f"{self.nombre} : {self.estado}")
+    def intentar_obtener_palillo(self,palillo):
+     palillo.acquire()
+     self.estado= f"Obtuvo {palillo}"
+     self.estado_label.config(text=f"{self.nombre} : {self.estado}")
+
+    def obtener_palillos(self):
+       self.estado="Intentando obtener palillo izquierdo"
+       self.estado_label.config(text=f"{self.nombre} : {self.estado}")
+       self.intentar_obtener_palillo(self.palillo_izquierdo)
+       self.estado="Intentando obtener palillo derecho"
+       self.estado_label.config(text=f"{self.nombre} : {self.estado}")
+       self.intentar_obtener_palillo(self.palillo_derecho)
+       self.estado= "Comiendo"
+       self.estado_label.config(text=f"{self.nombre} : {self.estado}")
 
