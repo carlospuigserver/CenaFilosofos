2c416bac23c79716af26fbed31043a8b8467a89f
carlospuigserver <cpuiggut@myuax.com>
2023-03-15 14:01:30 +0100

    funcion pensar

--- interfazGrafica.py
+++ interfazGrafica.py
@@ -17,3 +17,8 @@ class Filosofo:
         self.comidas=0
         self.comidas_label= tk.Label(self.interfaz, text=f"Comidas: {self.comidas}")
         self.comidas_label.place(x=self.x+180,y=self.y)
+
+def pensar(self):
+    self.estado="Pensando"
+    self.estado_label.config(text=f"{self.nombre} : {self.estado}")
+    time.sleep(random.uniform(1,5))
